8b6beb4b012596f98e8c0f0ebc8ef3a497d5a557
Simonhunor5 <79147758+Simonhunor5@users.noreply.github.com>
2021-03-30 16:15:13 +0300

    Int_Array_gyakorolo

---
+++ Labor06/Linked_List/functions.h
@@ -0,0 +1,35 @@
+//
+// Created by Huni on 3/30/2021.
+//
+
+#ifndef LINKED_LIST_FUNCTIONS_H
+#define LINKED_LIST_FUNCTIONS_H
+
+#include <stdbool.h>
+
+typedef struct Dig {
+    int vdig; // maga a szamjegy
+    int cdig; // a szamjegy elofordulasanak a szama
+}Dig;
+
+typedef struct Digit {
+    int nrdigit;
+    Dig* digits;
+}Digit;
+
+//Digit típus létrehozása
+Digit* Create(int capacity);
+//Digit típus felszabadítása
+void Destroy(Digit* array);
+//Tömb feltöltése
+void Input(Digit** array, int number);
+//k. számjegy lekérdezése
+int Digit_k(Digit* array, int k);
+//számjegyek számának lekérdezése
+int SizeDigit(Digit* array);
+//IntArray típus kiiratása
+void Print(Digit* array);
+int init(Digit* array, int digit);
+bool asc(Digit* array);
+
+#endif //LINKED_LIST_FUNCTIONS_H
